f03082ee074f4cf7990a0b1693da1136a4a5bd64
Alex Drummond <alexdrummond@Macintosh-2.(none)>
2008-01-23 17:59:26 +0000

    Tidy up CSS

--- js_includes/separator.js
+++ js_includes/separator.js
@@ -3,6 +3,7 @@ Separator.obligatory = [];
 function Separator(div, options, finishedCallback) {
     this.name = "Separator";
 
+    div.className = "next-item-message";
     div.appendChild(document.createTextNode(options.dget("message", "Press space to continue")));
 
     this.handleKey = function(code, time) {
