ea6f13615254e9117e2f7468a006a4fbd93d35cc
Alex Drummond <alexdrummond@Macintosh-2.local>
2008-01-28 14:11:39 +0000

    More accurate default Separator message.

--- js_includes/separator.js
+++ js_includes/separator.js
@@ -4,7 +4,7 @@ function Separator(div, options, finishedCallback) {
     this.name = "Separator";
 
     div.className = "next-item-message";
-    div.appendChild(document.createTextNode(options.dget("message", "Press space to continue")));
+    div.appendChild(document.createTextNode(options.dget("message", "Press any key to continue")));
 
     this.handleKey = function(code, time) {
         finishedCallback(null);
