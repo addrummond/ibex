d6abf4731a79051efe9a3fc0717b002ca7fa7713
Alex Drummond <alexdrummond@Macintosh-2.local>
2008-02-11 16:31:11 -0500

    Update mkdist.sh

--- mkdist.sh
+++ mkdist.sh
@@ -4,4 +4,7 @@ if [ ! -d dist ]; then
     mkdir dist
 fi
 
-tar -cz LICENSE conf.js css_includes/*.css js_includes/*.js json.js json.py main.js mkdist.sh server.py server_conf.py shuffle.js spr.html util.js util.py > dist/webspr-${1}.tar.gz
+O=`echo $PWD | awk 'BEGIN { FS="/"; } { print $NF }'`
+cd ..
+
+tar -cz $O/LICENSE $O/conf.js $O/css_includes/*.css $O/js_includes/*.js $O/json.js $O/json.py $O/main.js $O/mkdist.sh $O/server.py $O/server_conf.py $O/shuffle.js $O/spr.html $O/util.js $O/util.py > $O/dist/webspr-${1}.tar.gz
