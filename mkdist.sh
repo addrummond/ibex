b0f5e11ac8095243ce3edb0d8975c45b9eb8a96e
Alex Drummond <a.d.drummond@gmail.com>
2008-03-09 16:43:01 -0400

    Add data_includes dir to mkdist.sh

--- mkdist.sh
+++ mkdist.sh
@@ -9,4 +9,4 @@ fi
 O=`echo $PWD | awk 'BEGIN { FS="/"; } { print $NF }'`
 cd ..
 
-tar -cz $O/LICENSE $O/conf.js $O/css_includes/*.css $O/js_includes/*.js $O/json.js $O/json.py $O/main.js $O/mkdist.sh $O/server.py $O/server_conf.py $O/shuffle.js $O/experiment.html $O/util.js $O/util.py > $O/dist/webspr-${1}.tar.gz $O/example_lighttpd.conf
+tar -cz $O/LICENSE $O/conf.js $O/css_includes/*.css $O/js_includes/*.js $O/data_includes/*.js $O/json.js $O/json.py $O/main.js $O/mkdist.sh $O/server.py $O/server_conf.py $O/shuffle.js $O/experiment.html $O/util.js $O/util.py > $O/dist/webspr-${1}.tar.gz $O/example_lighttpd.conf
