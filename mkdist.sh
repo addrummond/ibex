28116dbd4ca9e0f99785e5162ad1f1273c4968aa
Alex Drummond <a.d.drummond@gmail.com>
2008-03-12 16:24:11 -0400

    Fix path error in mkdist.sh

--- mkdist.sh
+++ mkdist.sh
@@ -9,4 +9,4 @@ fi
 O=`echo $PWD | awk 'BEGIN { FS="/"; } { print $NF }'`
 cd ..
 
-tar -cz $O/LICENSE $O/www/conf.js $O/css_includes/*.css $O/js_includes/*.js $O/data_includes/*.js $O/www/json.js $O/mkdist.sh $O/www/server.py $O/server_conf.py $O/www/shuffle.js $O/www/experiment.html $O/overview.html $O/www/util.js $O/example_lighttpd.conf $O/docs/manual.txt > $O/dist/webspr-${1}.tar.gz 
+tar -cz $O/LICENSE $O/www/conf.js $O/css_includes/*.css $O/js_includes/*.js $O/data_includes/*.js $O/www/json.js $O/mkdist.sh $O/www/server.py $O/server_conf.py $O/www/shuffle.js $O/www/experiment.html $O/www/overview.html $O/www/util.js $O/example_lighttpd.conf $O/docs/manual.txt > $O/dist/webspr-${1}.tar.gz 
