a100a5c9d92e3b507ca321ea696f0d6584fcb3b9
Alex Drummond <a.d.drummond@gmail.com>
2013-05-17 15:24:09 -0400

    Minor.

--- js_includes/FlashSentence.js
+++ js_includes/FlashSentence.js
@@ -38,14 +38,11 @@ jqueryWidget: {
             if (sm) {
                 if (self.options.audioMessage) {
                     if (typeof(self.options.audioMessage) != "string") {
-                        alert("HH");
                         self.sentenceDom = htmlCodeToDOM(self.options.audioMessage);
                     }
                     else {
-                        alert("YY");
                         self.sentenceDom = $("<div>").text(self.options.audioMessage);
                     }
-                    alert(self.sentenceDom);
                 }
                 var name = self.options.s.audio;
                 var url = __server_py_script_name__ + '?resource=' + escape(name);
